38bdbacd7eeb7c6b19236ac9d54f5cb96fd4a8c3
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-17 16:02:35 -0800

    test run Maven

--- src/main/java/game/snake/FoodType.java
+++ src/main/java/game/snake/FoodType.java
@@ -1,5 +1,18 @@
 package game.snake;
 
+import javafx.scene.image.Image;
+
 public enum FoodType {
-    
+    APPLE("/game/snake/apple.png", 1); 
+
+    private Image image;
+    private int score;
+
+    FoodType(String imagePath, int score){
+        this.image = new Image(FoodType.class.getResourceAsStream(imagePath));
+        this.score = score;
+    }
 }
+
+
+
