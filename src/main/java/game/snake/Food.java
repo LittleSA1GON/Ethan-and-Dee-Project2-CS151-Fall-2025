12b8e685b05a2e580679ec6eb03d3cc12af59b96
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-17 16:28:33 -0800

    Implement Food to use to scatter multiple food items on the scene

--- src/main/java/game/snake/Food.java
+++ src/main/java/game/snake/Food.java
@@ -2,10 +2,18 @@ package game.snake;
 
 public class Food {
     
-    private String color;
-    private int x;
-    private int y;
-    
+    private FoodType foodType;
+    private int row;
+    private int column;
 
-    
+
+    public Food(FoodType foodType, int row, int column){
+        this.foodType = foodType;
+        this.row = row;
+        this.column = column;
+    }
+
+    public FoodType getFoodType(){ return this.foodType; }
+    public int getRow() { return this.row; }
+    public int getColumn() { return this.column; }
 }
--- src/main/java/game/snake/FoodType.java
+++ src/main/java/game/snake/FoodType.java
@@ -3,15 +3,31 @@ package game.snake;
 import javafx.scene.image.Image;
 
 public enum FoodType {
-    APPLE("/game/snake/apple.png", 1); 
 
-    private Image image;
-    private int score;
+    FISH_BONES("/game/snake/fish-bones.png", -5),
+    STRAWBERRY("/game/snake/strawberry.png", 1),
+    ORANGE("/game/snake/orange.png", 2),
+    RICE_BALL("/game/snake/rice-ball.png", 3),
+    APPLE("/game/snake/apple.png", 4),
+    SUSHI_EGG("/game/snake/sushi-egg.png", 5),
+    PUMPKIN("/game/snake/pumpkin.png", 6),
+    SODA("/game/snake/soda.png", 7),
+    ICE_CREAM("/game/snake/ice-cream.png", 8),
+    DONUT_CHOCOLATE("/game/snake/donut-chocolate.png", 9),
+    TACO("/game/snake/taco.png", 10),
+    PIZZA("/game/snake/pizza.png", 11);
+
+
+    private final Image image;
+    private final int score;
 
     FoodType(String imagePath, int score){
         this.image = new Image(FoodType.class.getResourceAsStream(imagePath));
         this.score = score;
     }
+
+    public Image getImage(){ return this.image; }
+    public int getScore(){ return this.score; }
 }
 
 
