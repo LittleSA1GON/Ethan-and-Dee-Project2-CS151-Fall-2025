5f79db9cf02bf05575428456264c8cce61d522b1
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-18 01:39:30 -0800

    Implements SnakeGame construtor to set up the root node that uses a VBox

--- src/main/java/game/snake/SnakeGame.java
+++ src/main/java/game/snake/SnakeGame.java
@@ -1,10 +1,61 @@
 package game.snake;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 public class SnakeGame {
 
+    private Label currScoreLabel = new Label(); //restart from 0 everytime game restarts -->use currScoreLabel.setText(String.valueOf(newScore)); to update it with real time
+    private VBox root; //top = toolbar, right beneath the toolbar = score (Hbox), the rest = canvas
+    private ToolBar toolBar;
+    private HBox scoreHBox;
+    private Canvas canvas;
 
-    public SnakeGame(){
+    /*
+     * TODO: Paste the ToolBar logic + UI and add it to the root (VBox)
+     */
 
+    public SnakeGame(){ 
+        this.toolBar = initToolBar(); //incorporating abstraction everywhere possible to make code readable
+        this.scoreHBox = initScoreHBox();
+        this.canvas = new Canvas(800, 520); //if let Stage Size be 800 x 600
+        this.root = constructRoot(toolBar, scoreHBox, canvas);
+    }//end of constructor
+
+    private ToolBar initToolBar(){ //initializing ToolBar (update this after completing game manager)
+        return new ToolBar(
+            new Button("Menu"),
+            new Button("More Button Coming")
+        );
+    }//end of initToolBar
+
+    private HBox initScoreHBox(){
+         HBox hBox = new HBox(15);
+         hBox.setPadding(new Insets(10));
+         hBox.setAlignment(Pos.TOP_RIGHT);
+
+         Label scoreLabel = new Label("Score: ");
+         currScoreLabel.setText(String.valueOf(0));
+         hBox.getChildren().addAll(scoreLabel, currScoreLabel);
+         return hBox;
+    } //end of initScoreHBox
+
+    private VBox constructRoot(ToolBar toolBar, HBox scoreHBox, Canvas canvas){
+        VBox vBox = new VBox(10);
+        vBox.setPadding(new Insets(15));
+
+        vBox.getChildren().addAll(toolBar, scoreHBox, canvas);
+        return vBox;
     }
-    
+
+    public VBox getRootNode(){ return this.root; }
+
+
 }
--- src/main/java/game/snake/SnakeGameApp.java
+++ src/main/java/game/snake/SnakeGameApp.java
@@ -1,1 +1,24 @@
 package game.snake;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SnakeGameApp extends Application{
+
+    @Override 
+    public void start(Stage primaryStage){
+        
+        SnakeGame snakeGame = new SnakeGame();
+
+        Scene scene = new Scene(snakeGame.getRootNode(), 800, 600);
+
+        primaryStage.setTitle("Snake Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }//end of start
+
+    public static void main(String[] args){
+        launch();
+    }
+}
