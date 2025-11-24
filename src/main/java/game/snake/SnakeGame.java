3c2985acb5a864abbad3456eeea6e136fe9d7093
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-24 00:56:11 -0800

    Add initSnake() to start with a snake with 3 segments positioned in the middle of the canvas

--- src/main/java/game/snake/SnakeGame.java
+++ src/main/java/game/snake/SnakeGame.java
@@ -1,5 +1,7 @@
 package game.snake;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -26,6 +28,7 @@ public class SnakeGame {
     private final int CELL_SIZE = 28; 
     private int[][] gridCanvas; 
     private Random random = new Random(); 
+    private List<SnakeBody> snake = new ArrayList<>();
 
     /*
      * TODO: Paste the ToolBar logic + UI and add it to the root (VBox)
@@ -57,7 +60,6 @@ public class SnakeGame {
     } //end of initScoreHBox
 
     private Canvas constructCanvas(){
-
         Canvas canvasL = new Canvas(700, 448);
         this.gc = canvasL.getGraphicsContext2D();
 
@@ -82,7 +84,7 @@ public class SnakeGame {
 
         vBox.getChildren().addAll(this.toolBar, this.scoreHBox, canvasHolderPane);
         return vBox;
-    }
+    }//end of constructRoot
 
     private Food createFood(){ 
         int[] rowColPair = new int [2];
@@ -94,7 +96,7 @@ public class SnakeGame {
         Food food = new Food(randomFoodType, row, col);
         
         return food;
-    }
+    }//end of createFood
 
     public void renderFood(Food food){
         //convert food position from grid coordinates to pixel
@@ -105,10 +107,29 @@ public class SnakeGame {
         int x = food.getColumn() * CELL_SIZE;
 
         gc.drawImage(food.getFoodType().getImage(), x, y, CELL_SIZE * cellSizeTimes, CELL_SIZE * cellSizeTimes);
+    }//end of renderFood
+
+    public void initSnake(){
+
+        int startRow = 8;
+        int startCol = 12;
+
+        SnakeBody head = new SnakeBody(startRow, startCol--);
+        snake.add(head);
+
+        for(int i = 0; i < 2; i++){
+            snake.add(new SnakeBody(startRow, startCol--));
+        }
+    }//end of initSnake
+
+    public void addSnakeBodyPart(){
+
+        SnakeBody tail = snake.get(snake.size() - 1);
+        //TODO: Continue here
+
     }
 
     public void startGame(){
-
         Food currFood = createFood();
         renderFood(currFood);
     }
