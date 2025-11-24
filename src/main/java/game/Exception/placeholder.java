645b1d91fef0e0dd4425203f7ab4ca765bd79724
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-24 00:11:49 -0800

    Updates package names to be in lowercase to make the program compile

--- src/main/java/game/Exception/placeholder.java
+++ src/main/java/game/Exception/placeholder.java
@@ -1,4 +1,4 @@
-package game.Exception;
+package game.exception;
 
 public class placeholder {
     
--- src/main/java/game/GameManager/placeholder.java
+++ src/main/java/game/GameManager/placeholder.java
@@ -1,4 +1,4 @@
-package game.GameManager;
+package game.gamemanager;
 public class placeholder {
     
 }
