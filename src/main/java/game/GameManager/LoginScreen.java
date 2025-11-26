ce4a402a9fde36aa7a01c59311c06fe891e2ce8e
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-26 02:04:35 -0800

    Set up LoginScreen UI

--- src/main/java/game/GameManager/LoginScreen.java
+++ src/main/java/game/GameManager/LoginScreen.java
@@ -1,14 +1,26 @@
 package game.gamemanager;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javafx.scene.control.TextField;
 import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class LoginScreen {
     
     private StackPane rootStackPane; 
     private Pane backgroundPane;
+    private VBox loginVBox;
+
+    private TextField usernameTF;
+    private TextField pwdTF;
+
+    private Button loginButton;
 
     public LoginScreen(){
         rootStackPane = new StackPane();
@@ -16,8 +28,10 @@ public class LoginScreen {
         constructBackgroundPane();
         rootStackPane.getChildren().add(this.backgroundPane);
 
-        dimBackgroundImage();
-    
+        dimAndBlurBackgroundImage();
+       
+        constructLoginVBox();
+        rootStackPane.getChildren().add( this.loginVBox);
     
 
     }
@@ -31,13 +45,56 @@ public class LoginScreen {
         );
     }
 
-    public void dimBackgroundImage(){
+    public void dimAndBlurBackgroundImage(){
         Pane dimOverlay = new Pane(); //to make background image dimmer
-        dimOverlay.setStyle("-fx-background-color: rgba(0, 0, 0, 0.25);"); 
+        dimOverlay.setStyle("-fx-background-color: rgba(0, 0, 0, 0.15);"); 
 
         GaussianBlur blur = new GaussianBlur(4);
         this.backgroundPane.setEffect(blur);
-        this.rootStackPane.getChildren().add(dimOverlay);
+        this.rootStackPane.getChildren().add(dimOverlay); //Be careful, this won't affect the background image if we add this before backgroundimage to the backgroundpane
+    }
+
+    public void constructLoginVBox(){
+        loginVBox = new VBox(15);
+        loginVBox.setPrefSize(300, 250);
+        loginVBox.setMaxSize(300, 250);
+        loginVBox.setAlignment(Pos.TOP_CENTER); //children centered horizontally
+        loginVBox.setPadding(new Insets(20));
+        loginVBox.setStyle(
+            "-fx-background-color: #c9f3ff;" +
+            "-fx-background-radius: 5;" //round corners
+        );
+
+        Label pleaseLogin = new Label("Please Login");
+        pleaseLogin.setStyle(
+            //"-fx-text-fill: ;" +
+            "-fx-font-size: 20px"
+        );
+
+        this.usernameTF = new TextField();
+        this.usernameTF.setPromptText("Username");
+        usernameTF.setStyle(
+            "-fx-background-color: #62ddff;" + 
+            "-fx-prompt-text-fill: black;"
+        );
+
+        this.pwdTF = new TextField();
+        this.pwdTF.setPromptText("Password");
+        this.pwdTF.setStyle(
+            "-fx-background-color: #62ddff;" +
+            "-fx-prompt-text-fill: black;"
+            
+        );
+
+        this.loginButton = new Button("Login");
+        this.loginButton.setMaxWidth(Double.MAX_VALUE); // fill the vBox width
+        this.loginButton.setStyle(
+            "-fx-background-color: #ff0066;" +
+            "-fx-text-fill: white;"
+        );
+
+        loginVBox.getChildren().addAll(pleaseLogin, usernameTF, pwdTF, loginButton);
+        
     }
 
     public StackPane getRootNode(){
