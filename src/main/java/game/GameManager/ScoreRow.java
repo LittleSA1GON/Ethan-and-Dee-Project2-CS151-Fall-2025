09d5408f3441129f329895fef8c9f39954d395c1
Dee <aeinhmu.naychi@sjsu.edu>
2025-11-27 16:39:58 -0800

    Set up ScoreRow to use in my TableView

---
+++ src/main/java/game/GameManager/ScoreRow.java
@@ -0,0 +1,20 @@
+package game.gamemanager;
+
+public class ScoreRow {
+
+    private int snakeGameScore;
+    private int blackjackScore;
+
+    public ScoreRow(int snakeGameScore, int blackjackScore){
+        this.snakeGameScore = snakeGameScore;
+        this.blackjackScore = blackjackScore;
+    }
+    
+    public Integer getSnakeGameScore(){ //javafx TableView's requires a getter that returns Integer (a reference type)
+        return this.snakeGameScore; //autoboxing int -> Integer
+    }
+
+    public Integer getBlackjackScore(){
+        return this.blackjackScore;
+    }
+}
